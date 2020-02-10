381a4eef36ed8b23c2b878ae12b6a4b0e6c2d940
sapae <57509402+sapae@users.noreply.github.com>
2020-02-10 19:25:20 +0000

    up

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -5,9 +5,10 @@ int	ft_strcmp(const char *s1, const char *s2)
 	int i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] != s2[i])
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
-	       return (s2[i] - s1[i]);
-	       i++;
+		i++;
 	}
+	return (s2[i] - s1[i]);
+	       
 }
